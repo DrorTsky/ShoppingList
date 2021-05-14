052fc12267b154fbd1342467471fcee4ff91068b
dror tartakovsky <dror.tartakovsky@s.afeka.ac.il>
2021-05-14 18:32:47 +0300

    initial commit

--- src/containers/TheLayout.js
+++ src/containers/TheLayout.js
@@ -1,19 +1,39 @@
-import React from "react";
+import React, { Component } from "react";
 import { TheContent, TheSidebar, TheFooter, TheHeader } from "./index";
+import Login from "../views/pages/login/Login";
 
-const TheLayout = () => {
-  return (
-    <div className="c-app c-default-layout">
-      <TheSidebar />
-      <div className="c-wrapper">
-        <TheHeader />
-        <div className="c-body">
-          <TheContent />
+export class TheLayout extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      isLoggedIn: true,
+    };
+    this.isLoggedInCheck = this.isLoggedInCheck.bind(this);
+  }
+
+  isLoggedInCheck = async (phoneNumber) => {
+    console.log("checking user");
+  };
+
+  render() {
+    var main = [];
+    main = this.state.isLoggedIn ? (
+      <div className="c-app c-default-layout">
+        <TheSidebar />
+        <div className="c-wrapper">
+          <TheHeader />
+          <div className="c-body">
+            <TheContent />
+          </div>
+          <TheFooter />
         </div>
-        <TheFooter />
       </div>
-    </div>
-  );
-};
+    ) : (
+      <Login isLoggedInCheck={this.isLoggedInCheck} />
+    );
+    return <div>{main}</div>;
+  }
+}
 
 export default TheLayout;
