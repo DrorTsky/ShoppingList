847aacb679815cba8aaaa2fdf7afe59dfb987ea1
dror tartakovsky <dror.tartakovsky@s.afeka.ac.il>
2021-05-15 15:58:54 +0300

    removes unnecessary

--- src/views/dashboard/Dashboard.js
+++ src/views/dashboard/Dashboard.js
@@ -3,6 +3,12 @@ import React, { Component } from "react";
 import AllShoppingLists from "../shopping_list_related/AllShoppingLists";
 
 export class Dashboard extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {};
+  }
+
   render() {
     return (
       <div>
